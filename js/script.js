955aa14af95551dd6c02e2075a551b8c9004f89b
TEPUY <contact@tepuy.site>
2025-09-17 19:28:13 -0400

    add clientes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Configuración de logos (fácil de actualizar)
-const clientLogosCount = 23; // Actualizar este número cuando añadas/quites logos
+const clientLogosCount = 25; // Actualizar este número cuando añadas/quites logos
 const brandLogosCount = 16;  // Actualizar este número cuando añadas/quites logos
 
 // Cargar logos dinámicamente
